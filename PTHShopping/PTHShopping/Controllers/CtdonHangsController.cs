68810e8f99ddf9114690d10112f834eec31eaad5
Cao Thanh Phong <thanhphongcao1601@gmail.com>
2021-12-10 15:47:22 +0700

    add đơn hàng

---
+++ PTHShopping/PTHShopping/Controllers/CtdonHangsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PTHShopping.Models;
+
+namespace PTHShopping.Controllers
+{
+    public class CtdonHangsController : Controller
+    {
+        private readonly PTHShoppingContext _context;
+
+        public CtdonHangsController(PTHShoppingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CtdonHangs
+        public async Task<IActionResult> Index()
+        {
+            var pTHShoppingContext = _context.CtdonHangs.Include(c => c.IddonHangNavigation).Include(c => c.IdsanPhamNavigation);
+            return View(await pTHShoppingContext.ToListAsync());
+        }
+
+        // GET: CtdonHangs/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ctdonHang = await _context.CtdonHangs
+                .Include(c => c.IddonHangNavigation)
+                .Include(c => c.IdsanPhamNavigation)
+                .FirstOrDefaultAsync(m => m.IdctdonHang == id);
+            if (ctdonHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(ctdonHang);
+        }
+
+        // GET: CtdonHangs/Create
+        public IActionResult Create()
+        {
+            ViewData["IddonHang"] = new SelectList(_context.DonHangs, "IddonHang", "IddonHang");
+            ViewData["IdsanPham"] = new SelectList(_context.SanPhams, "IdsanPham", "IdsanPham");
+            return View();
+        }
+
+        // POST: CtdonHangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdctdonHang,IddonHang,IdsanPham,SoDonHang,SoLuong,KhuyenMai,Tong,NgayGiaoHang")] CtdonHang ctdonHang)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ctdonHang);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IddonHang"] = new SelectList(_context.DonHangs, "IddonHang", "IddonHang", ctdonHang.IddonHang);
+            ViewData["IdsanPham"] = new SelectList(_context.SanPhams, "IdsanPham", "IdsanPham", ctdonHang.IdsanPham);
+            return View(ctdonHang);
+        }
+
+        // GET: CtdonHangs/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ctdonHang = await _context.CtdonHangs.FindAsync(id);
+            if (ctdonHang == null)
+            {
+                return NotFound();
+            }
+            ViewData["IddonHang"] = new SelectList(_context.DonHangs, "IddonHang", "IddonHang", ctdonHang.IddonHang);
+            ViewData["IdsanPham"] = new SelectList(_context.SanPhams, "IdsanPham", "IdsanPham", ctdonHang.IdsanPham);
+            return View(ctdonHang);
+        }
+
+        // POST: CtdonHangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("IdctdonHang,IddonHang,IdsanPham,SoDonHang,SoLuong,KhuyenMai,Tong,NgayGiaoHang")] CtdonHang ctdonHang)
+        {
+            if (id != ctdonHang.IdctdonHang)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ctdonHang);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CtdonHangExists(ctdonHang.IdctdonHang))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IddonHang"] = new SelectList(_context.DonHangs, "IddonHang", "IddonHang", ctdonHang.IddonHang);
+            ViewData["IdsanPham"] = new SelectList(_context.SanPhams, "IdsanPham", "IdsanPham", ctdonHang.IdsanPham);
+            return View(ctdonHang);
+        }
+
+        // GET: CtdonHangs/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ctdonHang = await _context.CtdonHangs
+                .Include(c => c.IddonHangNavigation)
+                .Include(c => c.IdsanPhamNavigation)
+                .FirstOrDefaultAsync(m => m.IdctdonHang == id);
+            if (ctdonHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(ctdonHang);
+        }
+
+        // POST: CtdonHangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var ctdonHang = await _context.CtdonHangs.FindAsync(id);
+            _context.CtdonHangs.Remove(ctdonHang);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CtdonHangExists(string id)
+        {
+            return _context.CtdonHangs.Any(e => e.IdctdonHang == id);
+        }
+    }
+}
---
+++ PTHShopping/PTHShopping/Controllers/DonHangsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PTHShopping.Models;
+
+namespace PTHShopping.Controllers
+{
+    public class DonHangsController : Controller
+    {
+        private readonly PTHShoppingContext _context;
+
+        public DonHangsController(PTHShoppingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DonHangs
+        public async Task<IActionResult> Index()
+        {
+            var pTHShoppingContext = _context.DonHangs.Include(d => d.IdkhachHangNavigation).Include(d => d.IdtrangThaiGiaoDichNavigation);
+            return View(await pTHShoppingContext.ToListAsync());
+        }
+
+        // GET: DonHangs/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var donHang = await _context.DonHangs
+                .Include(d => d.IdkhachHangNavigation)
+                .Include(d => d.IdtrangThaiGiaoDichNavigation)
+                .FirstOrDefaultAsync(m => m.IddonHang == id);
+            if (donHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(donHang);
+        }
+
+        // GET: DonHangs/Create
+        public IActionResult Create()
+        {
+            ViewData["IdkhachHang"] = new SelectList(_context.KhachHangs, "IdkhachHang", "IdkhachHang");
+            ViewData["IdtrangThaiGiaoDich"] = new SelectList(_context.TrangThaiGiaoDiches, "IdtrangThaiGiaoDich", "IdtrangThaiGiaoDich");
+            return View();
+        }
+
+        // POST: DonHangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IddonHang,IdkhachHang,NgayDatHang,NgayGiaoHang,IdtrangThaiGiaoDich,Deleted,DaThanhToan,NgayThanhToan,IdthanhToan,GhiChu")] DonHang donHang)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(donHang);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdkhachHang"] = new SelectList(_context.KhachHangs, "IdkhachHang", "IdkhachHang", donHang.IdkhachHang);
+            ViewData["IdtrangThaiGiaoDich"] = new SelectList(_context.TrangThaiGiaoDiches, "IdtrangThaiGiaoDich", "IdtrangThaiGiaoDich", donHang.IdtrangThaiGiaoDich);
+            return View(donHang);
+        }
+
+        // GET: DonHangs/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var donHang = await _context.DonHangs.FindAsync(id);
+            if (donHang == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdkhachHang"] = new SelectList(_context.KhachHangs, "IdkhachHang", "IdkhachHang", donHang.IdkhachHang);
+            ViewData["IdtrangThaiGiaoDich"] = new SelectList(_context.TrangThaiGiaoDiches, "IdtrangThaiGiaoDich", "IdtrangThaiGiaoDich", donHang.IdtrangThaiGiaoDich);
+            return View(donHang);
+        }
+
+        // POST: DonHangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("IddonHang,IdkhachHang,NgayDatHang,NgayGiaoHang,IdtrangThaiGiaoDich,Deleted,DaThanhToan,NgayThanhToan,IdthanhToan,GhiChu")] DonHang donHang)
+        {
+            if (id != donHang.IddonHang)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(donHang);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DonHangExists(donHang.IddonHang))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdkhachHang"] = new SelectList(_context.KhachHangs, "IdkhachHang", "IdkhachHang", donHang.IdkhachHang);
+            ViewData["IdtrangThaiGiaoDich"] = new SelectList(_context.TrangThaiGiaoDiches, "IdtrangThaiGiaoDich", "IdtrangThaiGiaoDich", donHang.IdtrangThaiGiaoDich);
+            return View(donHang);
+        }
+
+        // GET: DonHangs/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var donHang = await _context.DonHangs
+                .Include(d => d.IdkhachHangNavigation)
+                .Include(d => d.IdtrangThaiGiaoDichNavigation)
+                .FirstOrDefaultAsync(m => m.IddonHang == id);
+            if (donHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(donHang);
+        }
+
+        // POST: DonHangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var donHang = await _context.DonHangs.FindAsync(id);
+            _context.DonHangs.Remove(donHang);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DonHangExists(string id)
+        {
+            return _context.DonHangs.Any(e => e.IddonHang == id);
+        }
+    }
+}
